f603ba64afb871f5c8c980e9a6589f441f34903d
Adrian <adrian@dbx.hu>
2012-02-21 11:23:23 +0100

    boom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DnNumwords::Application.config.secret_token = '3c9a49dadc8ce7af988407cc7d5a2fb137d3b83fbb8f634d512fc6a0901fde7f74a2656a19c37aa54b7a75a05f767cdc788daf7a86550a52bdfaa079ec6b152f'
