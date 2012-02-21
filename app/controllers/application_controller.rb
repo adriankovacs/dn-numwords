423c14ddd8e935845876f85cd792ab4ed7638a53
adrian <az.adrian.kovacs@gmail.com>
2012-02-21 12:04:45 +0100

    form előkészítése

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
     render
   end
+
+  def get_numwords
+    @result = "Ez az eredmény."
+    render "application/index"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 DnNumwords::Application.routes.draw do
   root :to => "application#index"
+  match "/get_numwords" => "application#get_numwords", :as => :get_numwords
 end
